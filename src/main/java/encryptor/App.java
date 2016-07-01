97372abe53e1dab43f40572ad368335b8bd0f8de
murad naseralden <murad naseralden>
2016-07-01 18:09:45 +0300

    exercie 1 completed

--- src/main/java/encryptor/App.java
+++ src/main/java/encryptor/App.java
@@ -1,13 +1,21 @@
 package encryptor;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main( String[] args ) {
+        ArrayList<myFile> myFiles = new ArrayList<myFile>();
+        ArrayList<String> myFilesPaths = new ArrayList<String>();
+        String act;
+        int idx;
+
+        Helper helper = new Helper();
+        act = helper.getUserInput("enter E/e for Encryption or D/d for Decryption");
+        idx = helper.getFilePathFromUser(myFiles,myFilesPaths,"please insert the path of the file");
+        System.out.println("idx: " + idx + "  act: " + act);
+        helper.doActionOnFile(myFiles,idx,act); //encrypt/decrypt the file
+
+
     }
 }
