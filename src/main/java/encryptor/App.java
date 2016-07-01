6a3f5d0cbb5357cab288dc46eb66ce1bdc26dba3
murad naseralden <murad naseralden>
2016-07-01 17:12:29 +0300

    creating maven project

---
+++ src/main/java/encryptor/App.java
@@ -0,0 +1,13 @@
+package encryptor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
