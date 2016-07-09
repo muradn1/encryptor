2234933ad235695ef21eefb01cb6ecf067e7e7ed
murad naseralden <murad naseralden>
2016-07-09 04:22:59 +0300

    made the code more readable and easy to add more classes for checking tests

--- src/test/java/encryptor/TestRunner.java
+++ src/test/java/encryptor/TestRunner.java
@@ -7,25 +7,33 @@ import org.junit.runner.notification.Failure;
  * Created by murad on 03/07/2016.
  */
 public class TestRunner {
-    public static void main(String[] args) {
 
-        //running the tests of the Helper class
-        Result result = JUnitCore.runClasses(HelperTest.class);
+    public static void runClassTests (java.lang.Class<?> class1) {
+        Result result = JUnitCore.runClasses(class1);
         int i = result.getRunCount();
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
+        String nameOfTheClass = class1.getSimpleName().substring(0,class1.getSimpleName().indexOf("Test"));
+        System.out.println("All the " + i + " tests of " + nameOfTheClass +" class were Successful: "+ result.wasSuccessful());
+    }
 
-            System.out.println("All the " + i + " tests of helper class were Successful: "+ result.wasSuccessful());
+    public static void main(String[] args) {
+
+        //running the tests of the Helper class
+        runClassTests(HelperTest.class);
 
         //running the tests of the CaesarAlgorithm class
-        result = JUnitCore.runClasses(CaesarAlgorithmTest.class);
-        i = result.getRunCount();
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
+        runClassTests(CaesarAlgorithmTest.class);
 
-        System.out.println("\nAll the " + i + " tests of CaesarAlgorithm class were Successful: "+ result.wasSuccessful());
+        //running the tests of the XorAlgorithm class
+        runClassTests(XorAlgorithmTest.class);
+
+        //running the tests of the MultiplicationAlgorithm class
+        runClassTests(MultiplicationAlgorithmTest.class);
+
+        //running the tests of the EncryptDecryptObserver class
+        runClassTests(EncryptDecryptObserverTest.class);
 
 
     }
