b99b13dbce35babb14be27152655b7c1fccd470d
murad naseralden <murad naseralden>
2016-07-11 23:59:42 +0300

    this class presents the encryption algorithm that carries his name I used generic in it

---
+++ src/main/java/encryptor/SplitAlgorithm.java
@@ -0,0 +1,68 @@
+package encryptor;
+
+/**
+ * Created by murad on 11/07/2016.
+ */
+public class SplitAlgorithm  extends EncryptDecryptObservable implements EncryptionAlgorithmsWithGeneric {
+
+    @Override
+    public <Alg1 extends EncryptDecryptObservable ,Alg2 extends EncryptDecryptObservable> void encrypt(Key key, myFile myfile, Alg1 algorithm1, Alg2 algorithm2){
+
+        encrypt_decrypt_start("Encryption");
+
+
+        byte key1 = key.getKey()[0];
+        byte key2 = key.getKey()[1];
+        byte[] copiedFileData = getTheFileData(myfile);
+
+        ///////////////////////////encrypt using XOR algorithm/////////////////////////////////
+        for(int i=0;i<copiedFileData.length;i++) { //encrypt the bytes in the copied byteArray
+            copiedFileData[i] =(i%2 == 0 ? algorithm1.applyEncrypt(key1,copiedFileData[i]) : algorithm1.applyEncrypt(key2,copiedFileData[i]));
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        createTheEncryptedFile(myfile,copiedFileData);
+        encrypt_decrypt_end("Encryption");
+
+    }
+
+    @Override
+    public <Alg1 extends EncryptDecryptObservable,Alg2 extends EncryptDecryptObservable> void decrypt(Key key, myFile myfile, Alg1 algorithm1, Alg2 algorithm2){
+
+        encrypt_decrypt_start("Decryption");
+
+        byte key1 = key.getKey()[0];
+        byte key2 = key.getKey()[1];
+        byte[] copiedFileData = getTheFileData(myfile);
+
+        ///////////////////////////decrypt using XOR algorithm/////////////////////////////////
+        for(int i=0;i<copiedFileData.length;i++) { //decrypt the bytes in the copied byteArray
+            copiedFileData[i] =(i%2 == 0 ? algorithm1.applyDecrypt(key1,copiedFileData[i]) : algorithm1.applyDecrypt(key2,copiedFileData[i]));
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        createTheDecryptedFile(myfile,copiedFileData);
+        encrypt_decrypt_end("Decryption");
+    }
+
+
+    @Override
+    public void encrypt(byte key, myFile myfile) {
+
+    }
+
+    @Override
+    public void decrypt(byte key, myFile myfile) {
+
+    }
+
+    @Override
+    public byte applyEncrypt(byte key, byte copiedByteFromFileData) {
+        return 0;
+    }
+
+    @Override
+    public byte applyDecrypt(byte key, byte copiedByteFromFileData) {
+        return 0;
+    }
+}
---
+++ src/test/java/encryptor/DoubleAlgorithmTest.java
@@ -0,0 +1,92 @@
+package encryptor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by murad on 11/07/2016.
+ */
+public class DoubleAlgorithmTest {
+
+    DoubleAlgorithm doubleAlgorithm;
+    @Mock
+    CaesarAlgorithm caesarAlgorithm;
+    @Mock
+    XorAlgorithm xorAlgorithm;
+    @Mock
+    myFile fileForEncrypt;
+    @Mock
+    myFile fileForDecrypt;
+    @Mock
+    myFile decryptedFile;
+    @Mock
+    Key key;
+
+    String pathOfFileForEncrypt;
+    String pathOfFileForDecrypt;
+    String pathOfDecryptedFile;
+    String pathForKey;
+
+    ByteArrayOutputStream baos;
+    PrintStream outBuff;
+    PrintStream oldOut;
+
+    @Before
+    public void setUp() {
+        key = new Key();
+        caesarAlgorithm = new CaesarAlgorithm();
+        xorAlgorithm = new XorAlgorithm();
+        baos = new ByteArrayOutputStream();
+        outBuff = new PrintStream(baos);
+        oldOut = System.out;
+        System.setOut(outBuff);
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(oldOut);
+        outBuff = null;
+        baos = null;
+        key = null;
+        caesarAlgorithm = null;
+        xorAlgorithm = null;
+    }
+
+    @Test
+    public void testDoubleAlgorithmEncryptionAndDecryption() {
+
+        doubleAlgorithm = new DoubleAlgorithm();
+
+        pathOfFileForEncrypt = "c:\\files\\file1.txt";
+        pathOfFileForDecrypt = "c:\\files\\file1.txt.encrypted";
+        pathOfDecryptedFile = "c:\\files\\file1_decrypted.txt";
+        pathForKey = "c:\\files";
+
+        fileForEncrypt = new myFile(pathOfFileForEncrypt);
+        key.generateNewKey(pathForKey);
+
+
+        doubleAlgorithm.<CaesarAlgorithm,XorAlgorithm>encrypt(key,fileForEncrypt,caesarAlgorithm,xorAlgorithm);
+
+        fileForDecrypt = new myFile(pathOfFileForDecrypt);
+
+        doubleAlgorithm.<CaesarAlgorithm,XorAlgorithm>decrypt(key,fileForDecrypt,caesarAlgorithm,xorAlgorithm);
+
+        decryptedFile = new myFile(pathOfDecryptedFile);
+
+
+        assertArrayEquals("the data of the source file and the decrypted file most be the same",fileForEncrypt.getFileData(),decryptedFile.getFileData());
+        assertThat("the data of the source file and the encrypted file are not the same",fileForEncrypt.getFileData(),not(equalTo(fileForDecrypt.getFileData())));
+
+    }
+}
